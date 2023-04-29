74f4d56f5d51b1ba8e93c61b7b70b1e7436695f0
Rehab1Sama <132164473+Rehab1Sama@users.noreply.github.com>
2023-04-29 23:02:23 +0000

    Update config.py Jezal123

--- config/config.py
+++ config/config.py
@@ -9,31 +9,31 @@ if os.path.exists("Internal"):
 
 aiohttpsession = aiohttp.ClientSession()
 que = {}
-admins = {}
+admins = {Ree1Ree}
 
 #------------------------ Important Stuff 🤎 -----------------------
 
-API_ID = int(getenv("API_ID", "8934899"))
-API_HASH = getenv("API_HASH", "bf3e98d2c351e4ad06946b4897374a1e")
-BOT_TOKEN = getenv("BOT_TOKEN", "5204315005:AAGUB2q0QafBUwjDPGRAOcruwhUdqyXVlz4")
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "600"))
-STRING_SESSION = getenv("STRING_SESSION", "AgAZiHdAd4EOX2_TFaotq0BvHmvyfNnuzW1BXj_K4vIjrDbv_c2Ii7HvAyDU-kjCHAQsEvvY-AsNtiGCQ000WasBQ6qvB4wV5Ez7XZQyNmqbe2Cyg0DUTLHGFqKx-cuZ6RuYIla3TFYNx02fRE1TK_cYmdZr2KC4PvH80JM_zSzCju99osQ8xTQGI7L4wsZmOGD7ACCZpbGt_3WM-nalZCq4gS37ubEmu1pCLc7GE5NVkyUYyG1fERP8LRisXehFuf5Ly-ZOZ8e98sr0tYBRKfLsTODFsGVWRVIYvyZ4KstWO9O54MAm5eKkasktNoBSv49KSMPiBrtmbiNFKBXTi8loAAAAAVYfLGoA")
-BOT_USERNAME = getenv("BOT_USERNAME", "LROBOT")
+API_ID = int(getenv("API_ID", "25646040"))
+API_HASH = getenv("API_HASH", "5e1ea380a54ced6eb364f6d10efbcb70")
+BOT_TOKEN = getenv("BOT_TOKEN", "5660372064:AAHEwKNeKUX-zNA0W0AVjcrFMzwvhiyrfFg")
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "1000"))
+STRING_SESSION = getenv("STRING_SESSION", "BABql3ire3jB5ykLZ2E9_aR-bkDa3UjufMMdssrEVhKTfkGBMsDD17aS2svgKqNiaFjjWCNP9iVeQvyjEAuHHicmmlT8qhPvNoAwJ9un-RBmmDgenDbxobPadRhwWtsC_oPFakxh2qLlkHedezjLChQbD1Ebzi9yoxAELq5aNCmRUTUINQ7IvwXAhN5v8yWRBs1TNIoEPyUxSWQG18Kc3J5NSs6jownghMmeqlb4CB9AElJHzqsssJIhZHA5bNhC18rE7krpGTtPgz0ixkJ0m0Ld3fQpHO_6n1g-MfTT90hoLoxyIgdO1QnqOSmKGVNBJz9FGjIMFkgVLUeZf5naifM5Q04b5AA-kjCHAQsEvvY-AsNtiGCQ000WasBQ6qvB4wV5Ez7XZQyNmqbe2Cyg0DUTLHGFqKx-cuZ6RuYIla3TFYNx02fRE1TK_cYmdZr2KC4PvH80JM_zSzCju99osQ8xTQGI7L4wsZmOGD7ACCZpbGt_3WM-nalZCq4gS37ubEmu1pCLc7GE5NVkyUYyG1fERP8LRisXehFuf5Ly-ZOZ8e98sr0tYBRKfLsTODFsGVWRVIYvyZ4KstWO9O54MAm5eKkasktNoBSv49KSMPiBrtmbiNFKBXTi8loAAAAAVYfLGoA")
+BOT_USERNAME = getenv("BOT_USERNAME", "Jezal123_bot")
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1854384004").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1129192420").split()))
 OWNER_ID = list(
-    map(int, getenv("OWNER_ID", "1854384004").split())
+    map(int, getenv("OWNER_ID", "1129192420").split())
 )  # Input type must be interger
 SERVER_PLAYLIST_LIMIT = int(getenv("SERVER_PLAYLIST_LIMIT", "30"))
 
 #•••••••••••••••••••••••• Mongodb Url Stuff & Loggroupid •••••••••••
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001847569598")) 
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001916225272")) 
 
 MONGODB_URL = getenv("MONGODB_URL", "mongodb+srv://veez:mega@cluster0.heqnd.mongodb.net/veez?retryWrites=true&w=majority")
 #________________________ Updates  & Music bot name________________
-NETWORK = getenv("NETWORK", "xl444")
-GROUP = getenv("GROUP", "xl444")
-BOT_NAME = getenv("BOT_NAME", "Music")
+NETWORK = getenv("NETWORK", "https://t.me/J_ezal")
+GROUP = getenv("GROUP", "https://t.me/rhbfh65")
+BOT_NAME = getenv("BOT_NAME", "Jezal")
 BANNED_USERS = filters.user()
 
 #************************* Image Stuff  ****************************
